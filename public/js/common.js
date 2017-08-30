80d85d71b1977ae657d30d9ae1730c4058c692c2
WJNmm <2427372199@qq.com>
2017-08-30 21:50:40 +0800

    模版引擎重构登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($){
+define(['jquery','template','cookie'],function($,template){
 	//NProgress.start();
 	//NProgress.done();
 	//控制左侧菜单的折叠和展开
@@ -28,10 +28,14 @@ define(['jquery','cookie'],function($){
 	//console.log(sessionId);
 	//获取登录信息
 	var loginInfo = $.cookie('loginInfo');
-	var info = JSON.parse(loginInfo);
-	console.log(info);
-	$('.profile img').attr('src',info.tc_avatar);
-	$('.profile h4').html(info.tc_name);
+	var info = loginInfo ? JSON.parse(loginInfo) : {};
+	//console.log(info);
+	var tplstr = '<div class="avatar img-circle"> <img src="{{tc_avatar}}"> </div> <h4>{{tc_name}}</h4>';
+	var html = template.render(tplstr,info);
+	$('.aside .profile').html(html);
+	//$('.profile img').attr('src',info.tc_avatar);
+	//$('.profile h4').html(info.tc_name);
+
 });
 
 
