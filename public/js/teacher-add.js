27fc545e927968d9870f78d2ee188bbce0457d5d
WJNmm <2427372199@qq.com>
2017-09-01 15:01:01 +0800

    添加表单验证插件

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,48 +1,74 @@
-define(['jquery','template','util','datepicker','language'],function($,template,util){
+define(['jquery', 'template', 'util', 'datepicker', 'language', 'validate'], function ($, template, util) {
     //设置菜单选中
     util.setMenu('/teacher/list');
     //获取编辑的讲师ID
     var tcId = util.qs('tc_id');
-    if(tcId) {
+    if (tcId) {
         //编辑操作（根据ID调用后台接口获取数据）
-        $.ajax ({
-            type:'get',
-            url:'/api/teacher/edit',
-            data:{tc_id:tcId},
-            dataType:'json',
-            success:function(data) {
+        $.ajax({
+            type: 'get',
+            url: '/api/teacher/edit',
+            data: {tc_id: tcId},
+            dataType: 'json',
+            success: function (data) {
                 //console.log(data);
                 //解析数据渲染页面
                 data.result.operate = '编辑讲师';
-                var html = template('teacherTpl',data.result);
+                var html = template('teacherTpl', data.result);
                 $('#teacherInfo').html(html);
                 //提交编辑讲师表单
                 submitForm('/api/teacher/update');
             }
 
         });
-    }else {
+    } else {
         //添加操作
-        var html = template('teacherTpl',{operate:'添加讲师', tc_gender : 1});
+        var html = template('teacherTpl', {operate: '添加讲师', tc_gender: 1});
         $('#teacherInfo').html(html);
         //提交添加讲师表单
         submitForm('/api/teacher/add');
     }
     //提交表单公用方法
     function submitForm(url) {
-        $('#teacherBtn').click(function(){
-            $.ajax({
-                type :'post',
-                url :url,
-                data : $('#teacherForm').serialize(),
-                dataType :'json',
-                success:function(data) {
-                    if(data.code == 200) {
-                        location.href = '/teacher/list';
-                    }
+        $('#teacherForm').validate({
+            sendForm: false,
+            valid: function () {
+                //console.log('ok');
+                //这里应该提交表单
+                
+            },
+            description:{
+                tc_name:{
+                    required:'请输入用户名',
+                    valid:'用户名可以使用'
+                },
+                tc_pass:{
+                    required:'请输入密码',
+                    pattern:'密码必须是6位数字',
+                    valid:'密码有效'
+                },
+                tc_join_date:{
+                    required:'请输入日期',
+                    valid:'日期有效'
                 }
-            });
+            }
         });
     }
+
+    //function submitForm(url) {
+    //    $('#teacherBtn').click(function(){
+    //        $.ajax({
+    //            type :'post',
+    //            url :url,
+    //            data : $('#teacherForm').serialize(),
+    //            dataType :'json',
+    //            success:function(data) {
+    //                if(data.code == 200) {
+    //                    location.href = '/teacher/list';
+    //                }
+    //            }
+    //        });
+    //    });
+    //}
 
 });
