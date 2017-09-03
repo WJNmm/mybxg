ab169a22890dbcdd984bd1e11c9fe9905749957d
WJNmm <2427372199@qq.com>
2017-09-04 01:13:09 +0800

    添加进度条和遮罩效果

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-define(['jquery','util'],function($,util){
+define(['jquery','util','state'],function($,util){
     //设置导航菜单高亮选中
     //$('.navs a[href="'+location.pathname+'"]').addClass('active');
     util.setMenu(location.pathname);
